4f2d4526ba50b2225c777a65d1fb2f9eeab50939
Ruben Karlsson <Ruben.Karlsson.extern@rwe.com>
2022-03-31 12:17:50 +0200

    fix: typo in filename

---
+++ README.md
@@ -0,0 +1,27 @@
+API Playground:
+https://serpapi.com/playground?q=landscapes+abstract&location=Austin%2C+Texas%2C+United+States&gl=us&hl=en
+
+# Instructions
+
+1. Register and make private key
+2. clone repo
+3. run the following in the CLI:
+
+   ```$ npx npm@7 install```
+
+4. create .env file in root directory of your project and add add the following: 
+
+    ```API_KEY=HERE_YOUR_API_KEY```
+
+5. customise the params for your query (https://serpapi.com/images-results)
+6. run:
+
+    ```$ node index.js```
+
+---
+
+Followed this: 
+
+- https://serpapi.com/search-api
+- https://www.npmjs.com/package/google-search-results-nodejs
+- https://medium.com/geekculture/scrape-google-images-with-python-f9a20cda1355
--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const fs = require("fs");
 require("dotenv").config();
+
+const fs = require("fs");
+const http = require("http");
+
 const SerpApi = require("google-search-results-nodejs");
-const search = new SerpApi.GoogleSearch(process.env.API_KEY);
+const search = new SerpApi.GoogleSearch(
+  "df5ea7eb88235ace765f2e26b937b5bb7cd5434abba40c2b946924bb10d16194"
+);
 
 const params = {
   q: "Abstract Landscapes",
-  hl: "en",
-  gl: "us",
-  google_domain: "google.com",
+  gl: "us", // country to use for the search
+  tbm: "isch",
 };
 
-const callback = function (data) {
-  fs.writeFile(path.join(dirname, "results.json"), data);
+const searchCallback = (data) => {
+  for (let image of data.images_results) {
+    const splitURL = image.original.split(".");
+    const imgFormat = "." + splitURL[splitURL.length - 1];
+    const filename = image.title.replace(/\s/g, "") + imgFormat;
+    download(image.original, filename, function () {
+      console.log(`downloaded and saved ${filename}`);
+    });
+  }
 };
 
-// Show result as JSON
-search.json(params, callback);
+const download = (url, filename, callback) => {
+  http
+    .request(url, function (response) {
+      var data = new Stream();
+
+      response.on("data", function (chunk) {
+        data.push(chunk);
+      });
+
+      response.on("end", function () {
+        fs.writeFileSync(filename + ".", data.read());
+      });
+    })
+    .end();
+};
+
+search.json(params, searchCallback);
